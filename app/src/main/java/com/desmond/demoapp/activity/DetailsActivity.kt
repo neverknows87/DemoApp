b854f5912a78130d070da9610179506a1181c72c
Desmond <desmond.yap@liquidpay.com>
2019-11-05 16:30:27 +0800

    Created skeleton and model classes

---
+++ app/src/main/java/com/desmond/demoapp/activity/DetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.desmond.demoapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.desmond.demoapp.R
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
